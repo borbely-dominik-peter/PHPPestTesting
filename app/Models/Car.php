4c2ebeb34a38cd91da8f45ff262a0319ed89c69b
janko-julianna <janko.julianna@students.jedlik.eu>
2026-01-14 20:28:13 +0100

    DELETE

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
+    use HasFactory;
     // protected $table = 'autok';
     // protected $primaryKey = 'car_id';
 
---
+++ tests/Feature/GetAllTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+test('GET /api/cars exists', function () {
+    $response = $this->get('/api/cars');
+    $response->assertStatus(200);
+});
+
+test('GET /api/cars returns with 200 status code', function () {
+    $response = $this->get('/api/cars');
+
+    $response->assertStatus(200);
+});
+
+test('GET /api/cars calls the relevant controller, checked by data not being null', function () {
+    $response = $this->get('/api/cars');
+    expect($response->json())->not()->toBeNull();
+});
+
+test('GET /api/cars returns data in a JSON array format', function () {
+    $response = $this->get('/api/cars');
+    $data = $response->json();
+    expect($data)->toBeArray();
+});
