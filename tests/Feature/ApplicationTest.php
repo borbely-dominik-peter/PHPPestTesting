d4c07c92da63a89cbe1d51978d9e0b3659485622
Kerpics-Katrin-Rebeka <kerpics.katrin.rebeka@students.jedlik.eu>
2026-01-16 08:09:38 +0100

    page_loads_test

--- tests/Feature/ApplicationTest.php
+++ tests/Feature/ApplicationTest.php
@@ -3,3 +3,16 @@
 test('homepage does not accept post requests', function () {
     $this->post('/')->assertStatus(405);
 });
+
+test('welcome view renders without errors', function () {
+    $response = $this->get('/');
+
+    $response->assertOk();
+    expect($response->getContent())->toBeString();
+});
+
+test('non existent page returns 404', function () {
+    $this->get('/this-page-does-not-exist')
+        ->assertStatus(404);
+});
+
